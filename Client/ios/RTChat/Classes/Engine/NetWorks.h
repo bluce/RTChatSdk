b2a858a009dcc8160f8a3170d4e6cf43d673c078
raymon_wang <wang3140@hotmail.com>
2014-08-10 15:35:38 +0800

    代码更新

--- Client/ios/RTChat/Classes/Engine/NetWorks.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "Engine.h"
-#include "ClientTask.h"
-#include "FileDownloader.h"
-#include "NetCommand.h"
-#include "NetConnection.h"
-
--- Client/ios/RTChat/Classes/Engine/Rect.cpp
+++
@@ -1,3 +0,0 @@
-#include "Rect.h"
-
-const RectF RectF::NULLRECT = RectF( 0.0f, 0.0f, 0.0f, 0.0f );
